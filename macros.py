6c11750accab8007e00b04f6d7413f62fc5a683d
anandijain <anandj@uchicago.edu>
2019-05-12 18:53:40 -0500

    scraper almost done

--- macros.py
+++ macros.py
@@ -1,6 +1,21 @@
+# macros.py for highly usable data
+
 data = '/data/'
+
 url = 'https://www.baseball-reference.com/teams/'
+
 games_suffix = '-schedule-scores.shtml'
 
-mlb_teams = ['ANA', 'ARI', 'ATL', 'BAL', 'ATL', 'BOS', 'LAD', 'ANA', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET', 'HOU', 'OAK', 'KCR', 'ANA', 'LAD', 'FLA', 'ATL', 'MIL', 'BAL', 'MIN', 'WSN', 'SFG', 'NYM', 'NYY', 'OAK', 'OAK', 'PHI', 'PIT', 'SDP', 'SFG', 'SEA', 'MIL', 'BAL', 'STL', 'TBD', 'TEX', 'TOR', 'WSN', 'MIN', 'TEX']
+mlb_teams_short = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CHW', 'CIN', 'CLE', 'COL', 'DET', 
+			'HOU', 'KCR', 'ANA', 'LAD', 'FLA', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK',
+			'PHI', 'PIT', 'SDP', 'SFG', 'SEA', 'STL', 'TBD', 'TEX', 'TOR', 'WSN']
  
+mlb_teams_full = ['Arizona Diamondbacks', 'Atlanta Braves', 'Baltimore Orioles', 
+				'Boston Red Sox', 'Chicago Cubs', 'Chicago White Sox', 'Cincinnati Reds', 
+				'Cleveland Indians', 'Colorado Rockies', 'Detroit Tigers', 'Houston Astros', 
+				'Kansas City Royals', 'Los Angeles Angels', 'Los Angeles Dodgers', 
+				'Miami Marlins', 'Milwaukee Brewers', 'Minnesota Twins', 'New York Mets', 
+				'New York Yankees', 'Oakland Athletics', 'Philadelphia Phillies', 
+				'Pittsburgh Pirates', 'San Diego Padres', 'San Francisco Giants', 
+				'Seattle Mariners', 'St. Louis Cardinals', 'Tampa Bay Rays', 
+				'Texas Rangers', 'Toronto Blue Jays', 'Washington Nationals']
--- mlbpbp_scraper.py
+++ mlbpbp_scraper.py
@@ -1,24 +1,37 @@
 import bs4 
 import requests as r
-
 import macros as m
-import open_functions as of
+import openers as of
 
-def get_teams_links():
-	page = of.open_page(m.url)
-	table = page.find('table', {'id': 'teams_active'})
-	tbody = table.tbody
-	links = tbody.find_all('a')
 
-	three_letters = []
-	for link in links:
-		three_letters.append(link['href'][7:10])
+def get_table(page, table_id):  # given bs4 page and table id, finds table using bs4. returns tbody
+	table = page.find('table', {'id': table_id})
+	return table.tbody
 
-	print(three_letters)
 
+def get_team_links():
+	page = of.page(m.url)
+	tobdy = get_table(page, 'teams_active')
+	rows = tbody.find_all('tr')
+	team_names = []
+	links = []
+	for row in rows:
+		if row.th == None:
+			continue
+		a = row.a
+		links.append(a['href'])
+	return links
 
 
 def team_schedule_urls(years, team):
+	urls = []
+	while years[0] < years[1]:
+		full_url = m.url + team + '/' + str(years[0]) + m.games_suffix
+		years[0] += 1
+		urls.append(full_url)
+	return urls
+
+def team_seasons():
 	urls = []
 	while years[0] < years[1]:
 		full_url = m.url + team + '/' + str(years[0]) + m.games_suffix
@@ -30,24 +43,16 @@ def team_schedule_urls(years, team):
 def get_pages(urls):
 	pages = []
 	for url in urls:
-		pages.append(of.open_page(url))
+		pages.append(of.page(url))
 	return pages
 
 
-def get_comments(html):
-	comments = soup.findAll(text=lambda text:isinstance(text, Comment))
+def get_comments(page):
+	comments = page.findAll(text=lambda text:isinstance(text, bs4.Comment))
 	return comments
 
 
 def get_boxscores(page):
-	table = page.find('table', {'id' : 'team_schedule'})
-	if table is None:
-		return None
-	try:		
-		tbody = table.tbody
-	except AttributeError:
-		return None
-
 	rows = []
 	for row in tbody:
 		suffix = row.find('td', {'data-stat': 'boxscore'})
@@ -56,22 +61,32 @@ def get_boxscores(page):
 	return rows
 
 
-def boxscore(url):
-	page = get_pages([url])
+def pbp(boxscore_url):  # given boxscore url, returns pbp table as soup
+	page = get_pages([boxscore_url])
 	comments = get_comments(page)
-	html = bs4.BeautifulSoup(comments[31])
+	pbp = bs4.BeautifulSoup(comments[31], 'html.parser')
+	return pbp 
 
 
-get_teams_links()
+years = [2010, 2019]
 
+def main(years):  # years is list
+	start_year = years[0]
+	teams = get_team_links()  # list of urls
+	for team in teams:
+		years[0] = start_year
+		while years[0] < years[1]
+			# TODO
+			season = 
+			years[0] += 1
 
 # if __name__ == '__main__':
 # 	years = [2010, 2019]
-# 	team = 'ARI'
+# 	# team = 'ARI'
 	
 # 	fn = '.' + m.data + 'ARI_BOXSCORE_LINKS' + '.csv'
 
-# 	file = of.open_file(fn)
+# 	file = of.file(fn)
 # 	urls = team_schedule_urls(years, team)
 
 # 	pages = get_pages(urls)
